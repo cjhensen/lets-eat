2d71da8c5e99137c31aeb1b3b75f3158e14a6428
Christian Hensen <christian.j.hensen@gmail.com>
2017-09-07 17:09:56 -0400

    test file in test directory

---
+++ test/restaurantSearch.spec.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const should = chai.should();
+
+// jsdom allows you to test features of a web browser and jquery
+const jsdom = require('jsdom');
+
+const {JSDOM} = jsdom;
+
+global.document = new JSDOM("");
+global.$ = require('jquery')(global.document.window);
+
+const restaurantSearchTmpl = require('../src/components/restaurantSearch/restaurantSearch-tmpl');
+const restaurantSearch = require('../src/components/restaurantSearch/restaurantSearch');
+
+describe('restaurantSearch', function() {
+  it('should do something', function() {
+  });
+
+  it('should do something else', function() {
+  });
+
+});
