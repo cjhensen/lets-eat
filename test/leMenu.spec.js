57da9dad56725631fc7df76e753d981b12b08d58
Christian Hensen <christian.j.hensen@gmail.com>
2017-09-24 16:49:36 -0400

    installed jsdom-simulant, but event clicking is still not calling my handlergit statusgit status

--- test/leMenu.spec.js
+++ test/leMenu.spec.js
@@ -6,6 +6,8 @@ chai.use(spies);
 const jsdom = require('jsdom');
 const {JSDOM} = jsdom;
 
+const simulant = require('jsdom-simulant');
+
 const dom = new JSDOM('<body><div id="le-app"></div></body>', {runScripts: "outside-only"});
 const jsdomWindow = dom.window;
 const jsdomDocument = dom.window.document;
@@ -18,7 +20,7 @@ const APP_CONTAINER = $(jsdomDocument).find('#le-app');
 
 const leMenu = require('../src/components/leMenu').leMenu;
 
-describe('leMenu', function() {
+describe.only('leMenu', function() {
   it('should render the component to the DOM', function() {
     leMenu.render(APP_CONTAINER);
     expect(APP_CONTAINER.find('.le-menu').length).to.equal(1);
@@ -29,10 +31,13 @@ describe('leMenu', function() {
       expect(APP_CONTAINER.find('.le-menu-toggle').text()).to.equal('MENU');
     });
 
-    xit('should show "X" text on button on menu open state', function() {
-      const element = APP_CONTAINER.find('button.le-menu-toggle');
+    it('should show "X" text on button on menu open state', function() {
+      const event = simulant(jsdomWindow, 'click');
+      // const element = APP_CONTAINER.find('.le-menu-toggle');
+      const element = jsdomDocument.getElementsByClassName('le-menu-toggle')[0];
       console.log(APP_CONTAINER.find('button.le-menu-toggle').text());
-
+      // APP_CONTAINER.find('.le-menu-toggle').trigger(event);
+      simulant.fire(element, 'click');
       console.log(APP_CONTAINER.find('button.le-menu-toggle').text());
     });
   });
