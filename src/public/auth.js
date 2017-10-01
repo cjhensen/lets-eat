a927f40d9c173c1d6bb8cb5506b2f5fdda1db079
Christian Hensen <christian.j.hensen@gmail.com>
2017-10-01 10:35:47 -0400

    index page styling done

--- src/public/auth.js
+++ src/public/auth.js
@@ -1,3 +1,4 @@
+// Expand login form
 $('.btn-login-open').on('click', function(event) {
   event.preventDefault();
   console.log('login click');
@@ -7,3 +8,18 @@ $('.btn-login-open').on('click', function(event) {
 // $('.btn-login-open').on('blur', function(event) {
 //   $('.le-login').toggleClass('login-expand');
 // });
+
+// Switch to signup form
+$('.signup-link').on('click', function(event) {
+  event.preventDefault();
+  console.log('signup link clicked');
+  $('.signup-form').toggleClass('signup-display');
+  $('.login-form').toggleClass('login-display');
+});
+
+$('.login-link').on('click', function(event) {
+  event.preventDefault();
+  console.log('login link clicked');
+  $('.login-form').toggleClass('login-display');
+  $('.signup-form').toggleClass('signup-display');
+});
